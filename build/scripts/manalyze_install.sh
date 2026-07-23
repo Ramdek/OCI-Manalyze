03b2ea1dfba970106ef54c9337be642e2a9cc27b
Ramdek <116740013+Ramdek@users.noreply.github.com>
2026-07-23 19:36:49 +0200

    feat: remove bad format yara rules (now fixed)

--- build/scripts/manalyze_install.sh
+++ build/scripts/manalyze_install.sh
@@ -2,24 +2,6 @@
 # It should not be executed alone.
 
 MALWARE_BIN="/build/malware/pe_example"
-MALFORMATTED_YARA_RULES="/build/malformatted_rules"
-YARA_FILE="/usr/local/share/manalyze/yara_rules/clamav.yara"
-
-_remove_yara_rule() {
-
-	local rule_name="${1}"
-
-	sed -i "/${rule_name}/,/^}/d" "${YARA_FILE}"
-}
-
-_clear_bad_format_yara_rules() {
-
-	printf "%s\n" "Cleaning malformatted yara rules"
-
-	while read -r rule; do
-		_remove_yara_rule "${rule}"
-	done < "${MALFORMATTED_YARA_RULES}"
-}
 
 _init_yara_cache() {
 
@@ -31,7 +13,6 @@ manalyze_install() {
 
 	printf "%s\n" "Installing manalyze"
 
-	_clear_bad_format_yara_rules
 	_init_yara_cache
 
 	mv /usr/local/bin/manalyze /usr/local/bin/manalyze.bin
